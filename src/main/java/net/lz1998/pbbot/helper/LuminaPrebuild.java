d25cf067d0f6e4e83a491eaf46be179719594116
hybridsakura <hybridsakura@outlook.com>
2021-09-02 14:47:04 +0800

    常规代码更新20210902 业务底层目前重构完成，正在等待逐步迁移2

--- src/main/java/net/lz1998/pbbot/helper/LuminaPrebuild.java
+++ src/main/java/net/lz1998/pbbot/helper/LuminaPrebuild.java
@@ -26,20 +26,27 @@ public class LuminaPrebuild {
             boolean atLumina_lm = atLumina_lm_1 || atLumina_lm_2;
 
             boolean atKeyword_1_1 = rawMessage.contains("@宫代泉里");
-            boolean atKeyword_1_2 = rawMessage.contains("<at qq=\"00000000\"/");
+            boolean atKeyword_1_2 = rawMessage.contains("<at qq=\" \"/");
             boolean atKeyword_2_1 = rawMessage.contains("@一直恐惧");
-            boolean atKeyword_2_2 = rawMessage.contains("<at qq=\"000000000\"/");
+            boolean atKeyword_2_2 = rawMessage.contains("<at qq=\" \"/");
             boolean atKeyword_3_1 = rawMessage.contains("@过往已既");
-            boolean atKeyword_3_2 = rawMessage.contains("<at qq=\"00000000\"/");
+            boolean atKeyword_3_2 = rawMessage.contains("<at qq=\" \"/");
             boolean atKeyword_4_1 = rawMessage.contains("@萌新");
             boolean atKeyword_4_2 = rawMessage.contains("@公主");
-            boolean atKeyword_4_3 = rawMessage.contains("<at qq=\"00000000\"/");
-            boolean atKeyword = atKeyword_1_1 || atKeyword_1_2 || atKeyword_2_1 || atKeyword_2_2 || atKeyword_3_1 || atKeyword_3_2 || atKeyword_4_1 || atKeyword_4_2 || atKeyword_4_3;
+            boolean atKeyword_4_3 = rawMessage.contains("<at qq=\" \"/");
+            boolean atKeyword_5_1 = rawMessage.contains("@L'enfer, c'est les autres");
+            boolean atKeyword_5_2 = rawMessage.contains("@ずっとこのまま");
+            boolean atKeyword_5_3 = rawMessage.contains("<at qq=\" \"/");
+            boolean atKeyword = atKeyword_1_1 || atKeyword_1_2 ||
+                    atKeyword_2_1 || atKeyword_2_2 ||
+                    atKeyword_3_1 || atKeyword_3_2 ||
+                    atKeyword_4_1 || atKeyword_4_2 || atKeyword_4_3 ||
+                    atKeyword_5_1 || atKeyword_5_2 || atKeyword_5_3;
 
 
             boolean atLumina = atLumina_basic || atLumina_miyako || atLumina_lm || atKeyword;
 
-            requireSetup.setRequireAtLumina(atLumina);
+            requireSetup.setDetectAtLumina(atLumina);
             return requireSetup;
         }else {
             System.err.println("setLuminaAtRule传入参数不得为空！");
